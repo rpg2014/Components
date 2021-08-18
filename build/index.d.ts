ceb6e6d8ec79b9cd546b382f20482c6bf99c070f
rpg2014 <rpg2014@users.noreply.github.com>
2021-08-17 20:38:04 -0700

    Added build files

--- build/index.d.ts
+++ build/index.d.ts
@@ -3,12 +3,4 @@ import ProgressBar from "./ProgressBar/ProgressBar";
 import SearchBar from "./SearchBar/SearchBar";
 import Skeleton from "./Skeleton/Skeleton";
 import Spinner from "./Spinner/Spinner";
-
-
-export { 
-    Spinner,
-    SearchBar,
-    Skeleton,
-    ProgressBar,
-    PlayPauseButton 
-};
+export { Spinner, SearchBar, Skeleton, ProgressBar, PlayPauseButton };
